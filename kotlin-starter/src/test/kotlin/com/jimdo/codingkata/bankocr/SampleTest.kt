96d2098c2a2db6ed77dfff0faf8de5bc16e0a3f7
Tobias Bales <hi@tobiasbales.dev>
2021-07-12 10:30:15 +0200

    Set up kotlin starter

---
+++ kotlin-starter/src/test/kotlin/com/jimdo/codingkata/bankocr/SampleTest.kt
@@ -0,0 +1,11 @@
+package com.jimdo.codingkata.bankocr
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class SampleTest {
+    @Test
+    fun `it works`() {
+       assertEquals(true, true)
+    }
+}
